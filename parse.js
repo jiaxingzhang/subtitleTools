c7d8362d1cda49ccf92c08c8ae897d14a4a1bd8b
Jiaxing Zhang <jiaxingzhang@Jiaxings-MBP.fios-router.home>
2020-08-28 02:42:42 -0400

    clean up

--- parse.js
+++ parse.js
@@ -1,61 +1,19 @@
-
-var myArgs = process.argv.slice(2);
-if (myArgs.length !== 2) {
-    console.error('Usage: node parse.js [raw subtitle json file] [subtitle control json file] ');
-}
-
-fs = require('fs');
-
 try {
-
-    // This json is directly retrieved from google cloud
-    // curl -X GET -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) https://videointelligence.googleapis.com/v1/projects/637000999429/locations/us-east1/operations/10391224840668257911 > Q6.json
-    // Before we can get the json file from the server, we need to upload the mp4 to google cloud and make sure
-    // it's accessible (e.g. public) and do create a request.json as such:
-    // the following:
-    // {
-    //   "inputUri": "URI",  <<- fill this out
-    //   "features": ["TEXT_DETECTION"],
-    //   "videoContext": {
-    //     "textDetectionConfig": {
-    //       "languageHints": ["en-US"] <<- make sure this is correct
-    //     }
-    //   }
-    // }
-    // Then, do the following in bash:
-    // 
-    // curl -X POST -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) -H "Content-Type: application/json; charset=utf-8" -d @request.json https://videointelligence.googleapis.com/v1/videos:annotate
-    // Don't forget the set up the credential:
-    // export GOOGLE_APPLICATION_CREDENTIALS="PATH-TO-CRED.json"
+    var myArgs = process.argv.slice(2);
+    if (myArgs.length !== 2) {
+	console.error('Usage: node parse.js [raw subtitle json] [subtitle control json] ');
+    }
     
+    fs = require('fs');    
     var data = fs.readFileSync(myArgs[0], 'utf8')
     var raw = JSON.parse(data);
     var trans = raw.response.annotationResults[0].speechTranscriptions;
 
     // this is all the words from the video    
     var wordlib = combine(trans);
-    // require('fs').writeFile(
-    // 	'./wordlib.json',
-    // 	JSON.stringify(wordlib),
-    // 	function (err) {
-    //         if (err) {
-    // 		console.error('Crap happens');
-    //         }
-    // 	}
-    // );
     
-    // console.log(wordlib);
-
     // sub file is what the actual subtitle lines you'd like to have
     // This is where you can control the length of each subtitle line
-    // @todo: can this be generated automatically?
-    // right now, this is half manually:
-    // 1st:
-    // cat Q6.json | jq '.response.annotationResults[].speechTranscriptions[].alternatives[].transcript' > Q6_sub.json
-    // Then, manually construct a json file such as:
-    // {"trans": ["line1", "line2"]}
-    
-    
     var subfile = fs.readFileSync(myArgs[1], 'utf8');
     var subf = JSON.parse(subfile).trans;
 
@@ -82,7 +40,7 @@ function wrap(s) {
 function duration(s) {
     var hr = Math.round(s/3600);
     var min = Math.round(s/60)%60;
-    var sec = Math.round(s%60); // @todo: round up or down? 
+    var sec = Math.round(s%60);
     return wrap(hr) + ":" + wrap(min) + ":" + wrap(sec) + ",000";
 }
 
@@ -100,24 +58,13 @@ function genSub(subs) {
     }
 }
 
+// combine all words of transcript into an array
 function combine(trans) {
     var res = []
     for (t of trans) {
 	res = res.concat(t.alternatives[0].words);
     }
     return res;
-}
-
-// Find which alternative is the correct one for the current subtitle line
-function getCorrectTran(text, trans) {
-    for (t of trans) {
-	var te = t.alternatives[0].transcript.trim();
-	if (te.includes(text)) {
-	    return t;
-	}
-    }
-    console.error("Error");
-    return trans[0];
 }
 
 // Given a subtitle line, create a sub object that contains the line and start and end time
@@ -129,35 +76,20 @@ function makeSubLib(text, words) {
     
     var sentence = "";
     var i = 0;
+
+    var error = true; // assuming failure
     for (w of words) {
 	i++;
 	sentence = sentence + " " + w.word;
 	if (text.trim().localeCompare(sentence.trim()) == 0) {
 	    sub.end = w.endTime;
+	    error = false;
 	    break;
 	}
     }
-    
-    // remove it in the words. This is in place removal
-    words.splice(0, i);
-    return sub;
-}
 
-function makeSub(text, tran) {
-    var words = tran.alternatives[0].words;
-    var sub = {text: "", start: "", end : ""};
-    sub.text = text;
-    sub.start = words[0].startTime;
-    
-    var sentence = "";
-    var i = 0;
-    for (w of words) {
-	i++;
-	sentence = sentence + " " + w.word;
-	if (text.trim().localeCompare(sentence.trim()) == 0) {
-	    sub.end = w.endTime;
-	    break;
-	}
+    if (error) {
+	throw new Error('failed to match subtitle sentence');
     }
     
     // remove it in the words. This is in place removal

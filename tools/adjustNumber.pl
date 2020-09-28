71b970f66622674946e1cdac452f2287a63c10b0
Jiaxing Zhang <jiaxingzhang@Jiaxings-MBP.fios-router.home>
2020-09-28 19:46:47 -0400

    added adjust number tool in tools

---
+++ tools/adjustNumber.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use Scalar::Util qw(looks_like_number);
+
+my $filename = $ARGV[0];
+
+open(my $fh, '<:encoding(UTF-8)', $filename)
+  or die "Could not open file '$filename' $!";
+
+my $size = 0;
+my $num = 1;
+while (my $row = <$fh>) {
+    chomp $row;
+    if ($size == 0 && looks_like_number($row)) {
+    	print "$num\n";
+	$num++;
+    } else {
+	print "$row\n";
+    }
+    $size = length($row);
+}
+
